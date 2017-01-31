c5d8bf51b7c7aaf3e61e680b693c1dc04aaeaaaf
Nikola Bradas <Nikola.Bradas@fdmgroup.com>
2017-01-31 17:12:57 +0000

    Demo of central controller routing to subpages

---
+++ src/main/java/com/fdmgroup/controller/MenuController.java
@@ -0,0 +1,57 @@
+package com.fdmgroup.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MenuController
+ */
+public class MenuController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MenuController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getParameter("action");
+		PrintWriter out = response.getWriter();
+		//out.println(action);
+		
+		switch (action) {
+		case "register":
+			response.sendRedirect("register.jsp");			
+			break;
+		case "login":
+			response.sendRedirect("login.jsp");
+			break;
+		case "getall":
+			response.sendRedirect("getall.jsp");
+			break;
+		default:
+			response.sendRedirect("error.jsp");
+			break;
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+
+<ul>
+	<li><a href="/myWebapp2/MenuController?action=register">register</a></li>
+	<li><a href="/myWebapp2/MenuController?action=login">login</a></li>
+	<li><a href="/myWebapp2/MenuController?action=getall">getall</a></li>
+</ul>
+</body>
+</html>
